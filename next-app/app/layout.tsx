89126ad2e5880b740bd6a1f172af3cd86b1b2a75
XenoNOno <xenonono36@gmail.com>
2026-09-05 01:19:48 +0600

    Fix formatting of Geist font configuration in layout.tsx

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
